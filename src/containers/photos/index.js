2e7f4dac65dd2dbcec5b2f3772b46ae8195d65c8
Thiago <thiagogcamp@gmail.com>
2021-12-06 15:36:18 -0300

    feat(photos): added a theme switcher

--- src/containers/photos/index.js
+++ src/containers/photos/index.js
@@ -1,76 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../style/global.scss";
 import { Icon } from "@iconify/react";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 
-const Photos = () => (
-  <div id={"light-theme"}>
-    <div id={"body"}>
-      <header>
-        <nav>
-          <Logo />
-          <ul>
-            <li>
-              <a href="/">photos</a>
-            </li>
-            <li>
-              <a href="about">sobre</a>
-            </li>
-            <button>
-              <Icon icon="bx:bxs-moon" height="20" /> <span>Modo Escuro</span>
-            </button>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <h2>Galeria Principal</h2>
-        <div id={"image-row"}>
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-        </div>
-        <div id={"image-row"}>
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-        </div>
-        <div id={"image-row"}>
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-        </div>
-        <div id={"image-row"}>
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-          <img alt="ola" />
-        </div>
-      </main>
-      <footer>
-        <nav>
-          <Logo />
-          <ul>
-            <li>
-              <a href="/">photos</a>
-            </li>
-            <li>
-              <a href="about">sobre</a>
-            </li>
-            <button>
-              <Icon icon="bx:bxs-moon" height="20" />
-            </button>
-          </ul>
-        </nav>
-      </footer>
-    </div>
-  </div>
-);
+const Photos = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const [globalTheme, setGlobalTheme] = useState("light-theme");
+  const [btnName, setBtnName] = useState("Modo Escuro");
+  const [iconName, setIconName] = useState("Modo Escuro");
 
+  function handleTheme() {
+    return setDarkMode(!darkMode);
+  }
+
+  useEffect(() => {
+    if (darkMode) {
+      setGlobalTheme("dark-theme");
+      setBtnName("Modo Claro");
+      setIconName("bx:bxs-sun");
+    } else {
+      setGlobalTheme("light-theme");
+      setBtnName("Modo Escuro");
+      setIconName("bx:bxs-moon");
+    }
+  }, [darkMode]);
+
+  return (
+    <div id={globalTheme}>
+      <div id={"body"}>
+        <header>
+          <nav>
+            <Logo />
+            <ul>
+              <li>
+                <a href="/">photos</a>
+              </li>
+              <li>
+                <a href="about">sobre</a>
+              </li>
+              <button onClick={handleTheme}>
+                <Icon icon={iconName} height="20" /> <span>{btnName}</span>
+              </button>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <h2>Galeria Principal</h2>
+          <div id={"image-row"}>
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+          </div>
+          <div id={"image-row"}>
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+          </div>
+          <div id={"image-row"}>
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+          </div>
+          <div id={"image-row"}>
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+            <img alt="ola" />
+          </div>
+        </main>
+        <footer>
+          <nav>
+            <Logo />
+            <ul>
+              <li>
+                <a href="/">photos</a>
+              </li>
+              <li>
+                <a href="about">sobre</a>
+              </li>
+              <button onClick={handleTheme}>
+                <Icon icon={iconName} height="20" />
+              </button>
+            </ul>
+          </nav>
+        </footer>
+      </div>
+    </div>
+  );
+};
 export default Photos;
