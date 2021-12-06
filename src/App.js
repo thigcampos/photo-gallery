eb83b58e02d08865cd03cf8d3584e9ba867359f1
Thiago <thiagogcamp@gmail.com>
2021-12-06 14:48:45 -0300

    feat(src): created route to photos page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Photos } from "./containers";
+
 function App() {
   return (
-    <body>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </body>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Photos />} />
+      </Routes>
+    </Router>
   );
 }
 
