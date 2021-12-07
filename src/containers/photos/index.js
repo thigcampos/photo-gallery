c2a9e2ef5686594b38cfcaca0bc74736c79e05e6
Thiago <thiagogcamp@gmail.com>
2021-12-07 16:38:43 -0300

    feat(photos): updated navigation links to be acessible through tab button

--- src/containers/photos/index.js
+++ src/containers/photos/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "../../style/global.scss";
 import { Icon } from "@iconify/react";
-import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { Logo } from "../../assets";
 
 const Photos = () => {
   const [darkMode, setDarkMode] = useState(false);
   const [globalTheme, setGlobalTheme] = useState("light-theme");
   const [btnName, setBtnName] = useState("Modo Escuro");
   const [iconName, setIconName] = useState("Modo Escuro");
+  const [logoColor, setLogoColor] = useState("#1C1C1E");
 
   function handleTheme() {
     return setDarkMode(!darkMode);
@@ -18,10 +19,12 @@ const Photos = () => {
       setGlobalTheme("dark-theme");
       setBtnName("Modo Claro");
       setIconName("bx:bxs-sun");
+      setLogoColor("#f1fafc");
     } else {
       setGlobalTheme("light-theme");
       setBtnName("Modo Escuro");
       setIconName("bx:bxs-moon");
+      setLogoColor("#1c1c1e");
     }
   }, [darkMode]);
 
@@ -30,15 +33,19 @@ const Photos = () => {
       <div id={"body"}>
         <header>
           <nav>
-            <Logo />
+            <Logo theme={logoColor} />
             <ul>
               <li>
-                <a href="/">photos</a>
+                <a href="/" tabindex="1">
+                  photos
+                </a>
               </li>
               <li>
-                <a href="about">sobre</a>
+                <a href="about" tabindex="2">
+                  sobre
+                </a>
               </li>
-              <button onClick={handleTheme}>
+              <button onClick={handleTheme} tabindex="3">
                 <Icon icon={iconName} height="20" /> <span>{btnName}</span>
               </button>
             </ul>
@@ -66,12 +73,16 @@ const Photos = () => {
             <Logo />
             <ul>
               <li>
-                <a href="/">photos</a>
+                <a href="/" tabindex="4">
+                  photos
+                </a>
               </li>
               <li>
-                <a href="about">sobre</a>
+                <a href="about" tabindex="5">
+                  sobre
+                </a>
               </li>
-              <button onClick={handleTheme}>
+              <button onClick={handleTheme} tabindex="6">
                 <Icon icon={iconName} height="20" />
               </button>
             </ul>
