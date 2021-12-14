7f1d3ef8177240c86338a01961472a47fabec1f2
Thiago <thiagogcamp@gmail.com>
2021-12-14 09:18:35 -0300

    feat(utils): created nextpage and prevpage functions, also did the necessaries changes in getimages function

--- src/containers/photos/utils/index.js
+++ src/containers/photos/utils/index.js
@@ -1,21 +1,14 @@
-export function getImages(pageIndex, perPage, setState) {
-  fetch(
-    `https://api.pexels.com/v1/curated?page=${pageIndex}&per_page=${perPage}`,
-    {
-      headers: {
-        Authorization:
-          "563492ad6f9170000100000144987a13b7ad460abf7f9e7dff3e7c4e",
-      },
-    }
-  )
+export function getImages(pageIndex, setState) {
+  fetch(`https://api.pexels.com/v1/curated?page=${pageIndex}&per_page=20`, {
+    headers: {
+      Authorization: "563492ad6f9170000100000144987a13b7ad460abf7f9e7dff3e7c4e",
+    },
+  })
     .then((resp) => {
       return resp.json();
     })
     .then((data) => {
-      let prevPage = data.prev_page;
-      let nextPage = data.next_page;
-      setState(data.photos);
-      return [prevPage, nextPage, setState];
+      return setState(data);
     });
 }
 
@@ -32,3 +25,11 @@ export const lightTheme = {
   iconName: "bx:bxs-moon",
   logoColor: "#1c1c1e",
 };
+
+export function nextPage(setState, state) {
+  return setState(state + 1);
+}
+
+export function prevPage(setState, state) {
+  return setState(state - 1);
+}
